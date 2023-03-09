09bc25e9618ff65c72a6afad518ec8dae456c63f
shtuupid <mohamedbarreh8@gmail.com>
2023-03-09 18:07:35 +0000

    added middleware to parse incoming requests

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,7 @@ const express = require("express");
 const path = require("path");
 const app = express()
 const port = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
