b6064c727ba890ef38008f70e103845f3880f4a1
shtuupid <mohamedbarreh8@gmail.com>
2023-03-09 18:11:21 +0000

    routes to db and public files, setting index.html as base url

--- Develop/server.js
+++ Develop/server.js
@@ -7,3 +7,13 @@ const port = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
